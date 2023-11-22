3531b0014867ea4a74cd19b7b565b74a4c2f5a15
karitthorn <56643494+karitthorn@users.noreply.github.com>
2023-11-22 09:43:21 +0700

    add Frontend ✨

---
+++ frontend/app/(edocs)/edoc/succeed/page.tsx
@@ -0,0 +1,18 @@
+export default function Succeed() {
+  return (
+    <>
+      <div className="flex flex-col min-h-screen bg-gradient-to-b from-white to-green-200 ">
+        <div className="flex-grow   items-center w-full flex flex-col p-2 justify-center">
+          <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+            <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl ">
+              ✅
+            </h1>
+            <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 ">
+              คุณตอบกลับเเล้ว
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
