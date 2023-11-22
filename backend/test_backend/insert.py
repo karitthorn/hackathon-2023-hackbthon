b116f4492501faa793867ba7915958929b82a1bd
Nitisarath <64013165+pllus@users.noreply.github.com>
2023-11-22 10:18:48 +0700

    backend
    backend

---
+++ backend/test_backend/insert.py
@@ -0,0 +1,19 @@
+data ={
+    'header' = '',
+    'year' = [1,2],
+    'fac' = ['econ','bus'],
+    'image' = 'encoded_string',
+    'tag' = ['tech'],
+    'text' = 'Economic'
+}
+# data for news
+
+data ={
+    'header' = '',
+    'year' = [1,2],
+    'fac' = ['econ','bus'],
+    'image' = 'encoded_string',
+    'tag' = ['tech'],
+    'text' = 'Economic'
+}
+
